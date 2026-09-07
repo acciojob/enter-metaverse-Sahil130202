adc16b0e9cd38f0f50b33b799e8bc11338ba61de
Sahil Nayak <sahilsony2002@gmail.com>
2026-09-07 11:42:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let fun = documnet.getElementById("status")
+let but = documnet.getElementById("enterBtn")
+
+fun.addEventListener("button", function(){
+	fun.innerHTML="<h1>Entered Metaverse</h1>"
+})
